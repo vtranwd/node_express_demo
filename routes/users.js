1b7e40cbcbdf889717e8dcad92c0974867797288
Victor Tran <vtranwd@gmail.com>
2021-06-02 00:25:53 -0700

    might need to install Postman to test routes

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const router = express.Router();
+
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+]
+
+router.get('/', (req, res) => {
+    console.log(users);
+
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    
+});
+
+export default router;
